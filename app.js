12d61aa7279868d0a018c1847edd863b5a75f13d
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-05-21 16:31:43 +0700

    faet(update) api update User

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express")
 const mysql = require("mysql2/promise")
+const bcrypt = require('bcrypt')
 const cors = require("cors")
 
 const app = express();
 const post = 8000
 
 app.use(express.json())
-
-app.use("/hello", (req, res) => {
-    res.send("WELCOME NONTHANAN")
-})
 
 let conn = null
 const connectMySql = async () => {
@@ -23,11 +20,13 @@ const connectMySql = async () => {
 }
 
 
-app.post("/user", async(req, res) => {
+app.post("/user", async (req, res) => {
     try {
-        const { username, password, email, phone, address  } = req.body
+        const { username, password, email, phone, address } = req.body
 
-        const userDate = { username, password, email, phone, address  }
+        const hash = await bcrypt.hash(password, 10)
+
+        const userDate = { username, password: hash, email, phone, address }
 
         const [results] = conn = await conn.query("INSERT INTO users SET ?", userDate)
         res.status(201).json({
@@ -38,6 +37,38 @@ app.post("/user", async(req, res) => {
         res.status(400).json({
             message: "Create fail",
             error
+        })
+    }
+})
+
+app.put('/user/:id', async(req, res) => {
+    try {
+        const userId = req.params.id
+        const { username, password, email, phone, address } = req.body
+
+        const [checkUser] = await conn.query("SELECT * FROM users WHERE id = ?", userId)
+        if(checkUser.length === 0){
+            res.status(404).json({
+                message: "User Not Found"
+            })
+        }
+
+        let hashPassword;
+        if(password){
+            hashPassword = await bcrypt.hash(password, 10)
+        }
+
+        const userDate = { username, password: hashPassword, email, phone, address }
+
+        const [results] = await conn.query("UPDATE users SET ? WHERE id = ?", [userDate, userId])
+        res.status(200).json({
+            message: "Update User Successful",
+            results
+        })
+    } catch (error){
+        res.status(400).json({
+            message: "Update User fail",
+            Error: error
         })
     }
 })
