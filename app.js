f3d35ef7e590f2ca0a222148a266081c6f8229de
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-05-24 16:52:57 +0700

    Revert "feat(update) Update  Code structure"
    This reverts commit 0d82fc91a0172fa7d246fac2ebead9cc9ba54378.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken")
 const cors = require("cors")
 
 const app = express();
-const port = 8000
+const post = 8000
 
 app.use(express.json())
 
@@ -18,23 +18,19 @@ app.use(session({
 }))
 
 let conn = null
-const connectMySql = async() => {
-    conn = await mysql.createPool({
+const connectMySql = async () => {
+    conn = await mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "root",
-        database: "practice-cred",
-        // การใช้ Pool
-        waitForConnections: true, //  ถ้าไม่มี connection ว่าง จะให้รอ (ไม่ล้มเหลวทันที)
-        connectionLimit: 10,      //  จำนวนสูงสุดของ connection ที่จะเปิดพร้อมกันใน pool
-        queueLimit: 0             //  จำนวนคำขอที่รอคิวได้ (0 = ไม่จำกัด)
+        database: "practice-cred"
     });
     console.log("connect ==> DB")
 }
 
 const oauthToken = (req, res, next) => {
-    try {
-        if (!req.session.userId) {
+    try{
+        if(!req.session.userId){
             return res.status(400).json({
                 message: "Unauthorized"
             })
@@ -51,7 +47,7 @@ app.post("/login", async (req, res) => {
 
         // หาอีเมล ว่ามีอยู่ จริง
         const [results] = await conn.query("SELECT * FROM users WHERE email = ?", [email])
-        if (results.length === 0) {
+        if(results.length === 0){
             return res.status(401).json({
                 message: "Login Fail Wrong Email Password"
             })
@@ -60,13 +56,13 @@ app.post("/login", async (req, res) => {
         // ตรวจสอบ ว่า password มีอยู่จริง
         const userData = results[0]
         const match = await bcrypt.compare(password, userData.password)
-        if (!match) {
+        if(!match){
             return res.status(401).json({
                 message: "Login Fail Wrong Email Password",
             })
         }
 
-
+        
         // สร้าง session
         req.session.userId = userData.id
         req.session.user = userData
@@ -91,8 +87,7 @@ app.post("/user", async (req, res) => {
 
         const userData = { username, password: hash, email, phone, address }
 
-        const [results] = await conn.query("INSERT INTO users SET ?", userData)
-
+        const [results] = conn = await conn.query("INSERT INTO users SET ?", userData)
         res.status(201).json({
             message: "Create Successful",
             results
@@ -100,6 +95,7 @@ app.post("/user", async (req, res) => {
     } catch (error) {
         res.status(400).json({
             message: "Create fail",
+            error
         })
     }
 })
@@ -111,15 +107,17 @@ app.put('/user/:id', async (req, res) => {
 
         const [checkUser] = await conn.query("SELECT * FROM users WHERE id = ?", userId)
         if (checkUser.length === 0) {
-            return res.status(404).json({
+            res.status(404).json({
                 message: "User not found"
             })
         }
 
-        const userData = { username, email, phone, address }
+        let hashPassword;
         if (password) {
-            userData.password = await bcrypt.hash(password, 10)
+            hashPassword = await bcrypt.hash(password, 10)
         }
+
+        const userData = { username, password: hashPassword, email, phone, address }
 
         const [results] = await conn.query("UPDATE users SET ? WHERE id = ?", [userData, userId])
         res.status(200).json({
@@ -129,6 +127,7 @@ app.put('/user/:id', async (req, res) => {
     } catch (error) {
         res.status(400).json({
             message: "Update User fail",
+            error
         })
     }
 })
@@ -143,6 +142,7 @@ app.get("/users", oauthToken, async (req, res) => {
     } catch (error) {
         res.status(400).json({
             message: "Get user fail",
+            error
         })
     }
 })
@@ -163,6 +163,7 @@ app.get("/user/:id", async (req, res) => {
     } catch (error) {
         res.status(400).json({
             message: "Get user fail",
+            error
         })
     }
 })
@@ -173,7 +174,7 @@ app.delete("/user/:id", async (req, res) => {
         const [checkUser] = await conn.query("SELECT * FROM users WHERE id = ?", userId)
         if (checkUser.length === 0) {
             res.status(404).json({
-                message: "User not found"
+                message: "User not fail"
             })
         }
 
@@ -185,6 +186,7 @@ app.delete("/user/:id", async (req, res) => {
     } catch (error) {
         res.status(404).json({
             message: "Delete user fail",
+            error
         })
     }
 })
@@ -192,7 +194,7 @@ app.delete("/user/:id", async (req, res) => {
 
 
 
-app.listen(port, async () => {
+app.listen(post, async () => {
     await connectMySql()
-    console.log(`Start Server ${port}`)
+    console.log(`Start Server ${post}`)
 })
