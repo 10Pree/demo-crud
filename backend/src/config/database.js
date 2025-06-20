b926c96c423b2ab1f1e34f182556ffa1a19a47f5
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-06-20 21:59:35 +0700

    refactor(api) update env

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2/promise")
-
+require('dotenv').config()
 let conn = null
 const connectMySql = async() => {
     conn = await mysql.createPool({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "practice-cred",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE,
         // การใช้ Pool
         waitForConnections: true, //  ถ้าไม่มี connection ว่าง จะให้รอ (ไม่ล้มเหลวทันที)
         connectionLimit: 10,      //  จำนวนสูงสุดของ connection ที่จะเปิดพร้อมกันใน pool
