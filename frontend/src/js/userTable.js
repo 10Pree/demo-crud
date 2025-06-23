c278e5732b65fbf5b1cbef140254699129289a08
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-06-23 16:49:47 +0700

    feat(ui) decorate ui index

--- frontend/src/js/userTable.js
+++ frontend/src/js/userTable.js
@@ -4,14 +4,14 @@ export const populateTable = (users) => {
 
     users.forEach(user => {
         const row = `
-                <tr class="border border-b-1">
+                <tr class="border-t-1">
                     <td class="px-6 py-4 text-lg">${user.username}</td>
                     <td class="px-6 py-4">${user.email}</td>
                     <td class="px-6 py-4">${user.phone}</td>
                     <td class="px-6 py-4">${user.address}</td>
-                    <td class="px-3 py-4 text-center">
-                        <button class="popup-open-update bg-indigo-600 px-3 py-2 mb-2 md:mb-0 rounded-lg text-white hover:bg-indigo-400" data-user-id="${user.id}"  type="button">Edit</button>
-                        <button class="btn-delete  bg-red-600 px-3 py-2 rounded-lg text-white hover:bg-red-400" data-user-id="${user.id}" type="button">Delete</button>
+                    <td class="px-3 py-4 text-center grid gap-1.5">
+                        <button class="popup-open-update inline-flex items-center justify-center align-middle select-none font-sans  text-center px-4 py-2 text-white text-sm font-medium rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/30 transition-all duration-300 before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased" data-user-id="${user.id}"  type="button">Edit</button>
+                        <button class="btn-delete  inline-flex items-center justify-center align-middle select-none font-sans  text-center px-4 py-2 text-white text-sm font-medium rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/30 transition-all duration-300 before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased" data-user-id="${user.id}" type="button">Delete</button>
                     </td>
                 </tr>
         `
