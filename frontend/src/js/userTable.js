e3e612c45f75b262b69730b0927a8c0b5c52db80
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-06-23 15:47:55 +0700

    feat(ui) responsive

--- frontend/src/js/userTable.js
+++ frontend/src/js/userTable.js
@@ -10,7 +10,7 @@ export const populateTable = (users) => {
                     <td class="px-6 py-4">${user.phone}</td>
                     <td class="px-6 py-4">${user.address}</td>
                     <td class="px-3 py-4 text-center">
-                        <button class="popup-open-update bg-indigo-600 px-3 py-2 rounded-lg text-white hover:bg-indigo-400" data-user-id="${user.id}"  type="button">Edit</button>
+                        <button class="popup-open-update bg-indigo-600 px-3 py-2 mb-2 md:mb-0 rounded-lg text-white hover:bg-indigo-400" data-user-id="${user.id}"  type="button">Edit</button>
                         <button class="btn-delete  bg-red-600 px-3 py-2 rounded-lg text-white hover:bg-red-400" data-user-id="${user.id}" type="button">Delete</button>
                     </td>
                 </tr>
